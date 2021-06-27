d53afa89e05ae88002c58a79c49ddf86b4ba5d5d
Siva Perumal K <sivaperumal644@gmail.com>
2021-06-27 18:13:01 +0530

    Remove Github test code.

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("register/", views.RegisterUserView.as_view(), name="register"),
     path("login/", views.LoginUserView.as_view(), name="login"),
     path("refresh_token", views.RefreshTokenView.as_view(), name="refresh_token"),
-    path("test", views.TestingView.as_view(), name="test_view"),
 ]
